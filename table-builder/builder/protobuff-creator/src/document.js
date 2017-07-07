4ecf03c37f8c4fb9c6b06a31832d745a568dc21f
YiJiangFengYun <yijiangfengyun@outlook.com>
2017-07-07 16:57:07 +0800

    1.Doc

--- table-builder/builder/protobuff-creator/src/document.js
+++ table-builder/builder/protobuff-creator/src/document.js
@@ -3,9 +3,13 @@
  * Document Type
  */
 let Base = require("./base");
+let Syntax = require("./syntax");
+let docVersion = require("./documentVersion");
 
 let Document = function (name, version, option) {
     Base.call(this, name, option);
+    docVersion.currVersion = version || docVersion.VERSION_3;
+    this.syntax = new Syntax("syntax");
 };
 
 Object.assign(Document.prototype, Base.prototype, {
