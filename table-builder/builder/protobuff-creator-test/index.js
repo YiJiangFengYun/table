65351feb0dd4d09ac0abf6363dda03da79d0f5f7
YiJiangFengYun <yijiangfengyun@outlook.com>
2017-07-08 15:25:00 +0800

    1.Create project: protobuff-creator-test

---
+++ table-builder/builder/protobuff-creator-test/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by YiJiangFengYun on 2017/7/8.
+ * test proto buffer creator.
+ */
+
+let protobuffCreator = require("./../protobuff-creator");
+
+let doc = protobuffCreator.createDoc("protobuff-creator-test", protobuffCreator.docVersion.VERSION_3, "test");
