d29156f0afd53f9fc2e046e5b6ba88845d6f1317
YiJiangFengYun <yijiangfengyun@outlook.com>
2017-07-28 20:43:45 +0800

    Change test content for protobuff-creator-test-py project.

--- table-builder/builder/protobuff-creator-test-py/test.py
+++ table-builder/builder/protobuff-creator-test-py/test.py
@@ -1,6 +1,6 @@
-from creator import pb_document
-from creator import pb_enum_doc_versions
-from creator.types import pb_buildInTypes
+from pb_creator import pb_document
+from pb_creator import pb_enum_doc_versions
+from pb_creator.types import pb_build_in_types
 
 doc = pb_document.Document("protobuff-creator-test",
                            pb_enum_doc_versions.DocVersion.VERSION_3,
@@ -11,8 +11,8 @@ enum1 = doc.add_enum("enum1-test")
 message1 = doc.add_message("message1-test")
 
 enum1.add_field("ENUM_1")
-message1.add_field("field", pb_buildInTypes.string)
-message1.add_field("fieldRepeat", pb_buildInTypes.int32, {"is_repeated": True})
+message1.add_field("field", pb_build_in_types.type_string)
+message1.add_field("fieldRepeat", pb_build_in_types.type_int32, {"is_repeated": True})
 
 message2 = doc.add_message("message2-test")
 message2.add_field("enum1", enum1)
