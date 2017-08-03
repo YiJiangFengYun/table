486ae5db6a7316f1f8a2a7f0774363ea1dce2a8a
YiJiangFengYun <yijiangfengyun@outlook.com>
2017-08-03 15:29:41 +0800

    Now protobuff-creator only support protobuff version 3.

--- table-builder/builder/protobuff-creator/pb_creator/pb_enum_doc_versions.py
+++ table-builder/builder/protobuff-creator/pb_creator/pb_enum_doc_versions.py
@@ -2,5 +2,4 @@ from enum import Enum
 
 
 class DocVersion(Enum):
-    VERSION_2 = 2
     VERSION_3 = 3
--- table-builder/builder/protobuff-creator/pb_creator/pb_syntax.py
+++ table-builder/builder/protobuff-creator/pb_creator/pb_syntax.py
@@ -3,12 +3,12 @@ from pb_creator.pb_enum_doc_versions import DocVersion
 
 
 class Syntax(base.Base):
-    def __init__(self, name: str, version:DocVersion = DocVersion.VERSION_3):
+    def __init__(self, name: str, version: DocVersion = DocVersion.VERSION_3):
         super(Syntax, self).__init__(name)
-        if version == DocVersion.VERSION_2:
-            self.version = "proto2"
-        else:
+        if version == DocVersion.VERSION_3:
             self.version = "proto3"
+        else:
+            raise ValueError("Syntax version error.")
 
     def to_text(self) -> str:
         return "syntax = \"" + self.version + "\";"
