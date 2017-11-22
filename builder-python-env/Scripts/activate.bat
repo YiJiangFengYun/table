472d8431ac20af51868a7d71894289f5a416ddd3
一江风云 <yijiangfengyun@outlook.com>
2017-11-22 21:09:46 +0800

    Change python virtual environment by python36.2

--- builder-python-env/Scripts/activate.bat
+++ builder-python-env/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=F:\my-work-space\common\table\builder-python-env2"
+set "VIRTUAL_ENV=F:\my-work-space\common\table\builder-python-env"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
@@ -14,7 +14,7 @@ if defined _OLD_VIRTUAL_PYTHONHOME (
 )
 
 set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
-set "PROMPT=(builder-python-env2) %PROMPT%"
+set "PROMPT=(builder-python-env) %PROMPT%"
 
 if defined PYTHONHOME (
     set "_OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%"
